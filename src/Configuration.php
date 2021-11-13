dde2e894ac69bbb911cc8fc29bf47b1ac0a6aeaf
XieXiLin158 <xiexilin158@qq.com>
2021-11-14 00:04:11 +0800

    不能用的验证

--- src/Configuration.php
+++ src/Configuration.php
@@ -1,11 +1,22 @@
 <?php
 
-namespace XXX;
+namespace CakeMC\Microsoft;
 
 class Configuration
 {
     public function render()
     {
-        return view('xxx::config');
+        $form = Option::form(
+            'mojang_verification_microsoft',
+            trans('CakeMC\Microsoft::config.score_config'),
+            function ($form) {
+                $form->text(
+                    'mojang_verification_score_award',
+                    trans('CakeMC\Microsoft::config.score_award')
+                )->placeholder(trans('CakeMC\Microsoft::config.default'))
+                    ->description(trans('CakeMC\Microsoft::config.description'));
+            })->handle();
+
+        return view('CakeMC\Microsoft::config', compact('form'));
     }
 }
